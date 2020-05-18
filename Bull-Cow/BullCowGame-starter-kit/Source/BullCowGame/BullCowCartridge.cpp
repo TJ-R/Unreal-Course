86c9711f6b59d5e4d0cacda9bb951973f9736734
Thomas <thomasrunyan13@gmail.com>
2020-05-18 14:58:45 -0400

    Created Setup game method and changed hard coded ints to be based of HiddenWord.Len()

--- Bull-Cow/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ Bull-Cow/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,26 +4,58 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    
+    SetupGame(); //Setting up the game
+
+    PrintLine(TEXT("The HiddenWord is: %s"), *HiddenWord);
+
+    //Welcome the player
     PrintLine(TEXT("Hi, welcome to the Bull Cows game."));
-
-    HiddenWord = TEXT("about");
-
-    PrintLine(TEXT("Guess the five letter word.")); //Change hard coded word length!!!
-    PrintLine(TEXT("Press enter to continue......"));
+    PrintLine(TEXT("Guess the %i letter word."), HiddenWord.Len());
+    PrintLine(TEXT("Type in your guess and"));
+    PrintLine(TEXT("press enter to continue......"));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    
+
+    //Check the player guess
     if(Input == HiddenWord)
     {
-        PrintLine(TEXT("You guessed the hidden word correctly."));
         PrintLine(TEXT("You win!"));
+        //Exit the game
     }
     else
     {
-        PrintLine(TEXT("You guessed the hidden word incorrectly."));
-        PrintLine(TEXT("You lose."));
+        if(HiddenWord.Len() != Input.Len())
+        {
+            PrintLine(TEXT("The Hidden Word is %i characters long,"), HiddenWord.Len());
+            PrintLine(TEXT("Please guess again......"));
+        }
+        else
+        {
+            PrintLine(TEXT("You lose."));
+        }
     }
+    // Check If Isogram
+    // Prompt to guess again.
+
+    // Remove a life
+
+    //Check If life is >0
+
+    //If yes prompt for new guess
+    //Show lives left
+
+    //If no print GameOver and show the hidden word
+    //Prompt user to play again, press enter
+    //Check user input
+    //PlayAgain or Quit
+}
+
+void UBullCowCartridge::SetupGame()
+{
+    HiddenWord = TEXT("about");
+    Lives = HiddenWord.Len();
 }
